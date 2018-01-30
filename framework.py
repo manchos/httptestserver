987d0c2ba14ac01a5edeaa695d2bf0821e2ddf0e
manchos <vladm.ru@gmail.com>
2018-01-30 22:52:48 +0300

    add status code

--- framework.py
+++ framework.py
@@ -1,8 +1,20 @@
+from http_status import Status
+
+
 def application(environ, start_response):
     print(environ)
     if environ['PATH_INFO'] == '/info/':
-        response_text = b'Contact page'
+        response_text, status_code, extra_headers = info_url_handler(environ)
     else:
         response_text = b'404'
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    status_code_message = '{}{}'.format(
+        Status(status_code).code,
+        Status(status_code).name
+    )
+    start_response(
+        status_code_message,
+        [('Content-Type', 'text/html')])
     return [response_text]
+
+def info_url_handler(environ):
+    return b'Contact page', 201, {}
