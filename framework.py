c06cbfa21ee0d637d3f336a75c1989e764556ed1
manchos <vladm.ru@gmail.com>
2018-01-30 23:38:41 +0300

    add response headers

--- framework.py
+++ framework.py
@@ -6,15 +6,26 @@ def application(environ, start_response):
     if environ['PATH_INFO'] == '/info/':
         response_text, status_code, extra_headers = info_url_handler(environ)
     else:
+        status_code = 404
+        extra_headers = {}
         response_text = b'404'
+
     status_code_message = '{}{}'.format(
         Status(status_code).code,
-        Status(status_code).name
+        Status(status_code).name,
     )
+    headers = {
+        'Content-Type': 'text/html',
+    }
+    headers.update(extra_headers)
+
     start_response(
         status_code_message,
-        [('Content-Type', 'text/html')])
+        list(headers.items()),
+    )
     return [response_text]
 
 def info_url_handler(environ):
-    return b'Contact page', 201, {}
+    return b'Contact page', 201, {'X-test-header': '123'}
+
+
