8d3253970a2cb9c27a8fbe7dc4d40560b0b40369
manchos <vladm.ru@gmail.com>
2018-01-30 21:30:42 +0300

    first wsgi initial

--- simple_server.py
+++ simple_server.py
@@ -2,11 +2,13 @@ import socket
 
 def server_forever():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9004))
+    server_socket.bind(('185.87.194.206', 9004))
     server_socket.listen()
+
     while True:
         client_connection, client_address = server_socket.accept()
         request = client_connection.recv(1024)
+        print(client_address)
         print(request)
 
 
