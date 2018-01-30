b5bc7f92642fdf87577f4e9f46a7cf295f28947a
manchos <vladm.ru@gmail.com>
2018-01-30 21:59:30 +0300

    add PATH_INFO /info/

--- framework.py
+++ framework.py
@@ -1,3 +1,8 @@
 def application(environ, start_response):
+    print(environ)
+    if environ['PATH_INFO'] == '/info/':
+        response_text = b'Contact page'
+    else:
+        response_text = b'404'
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b"Hello World from a simple WSGI application"]
+    return [response_text]
