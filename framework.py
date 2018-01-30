021b5e5e25c6cc6fac497a059f030ab4fe3c8e04
manchos <vladm.ru@gmail.com>
2018-01-31 00:22:25 +0300

    refactoring, use class APP for routing manage

--- framework.py
+++ framework.py
@@ -1,35 +1,6 @@
 from http_status import Status
 
 
-def application(environ, start_response):
-    # print(environ)
-
-    handlers = {
-        '/': index_url_handler,
-        '/info/': info_url_handler,
-    }
-    current_url_handler = handlers.get(
-        environ['PATH_INFO'],
-        not_found_handler
-    )
-    response_text, status_code, extra_headers = current_url_handler(environ)
-
-    status_code_message = '{}{}'.format(
-        Status(status_code).code,
-        Status(status_code).name,
-    )
-    headers = {
-        'Content-Type': 'text/html',
-    }
-    headers.update(extra_headers)
-
-    start_response(
-        status_code_message,
-        list(headers.items()),
-    )
-    return [response_text]
-
-
 def index_url_handler(environ):
     return b'Index page', 200, {}
 
@@ -40,3 +11,38 @@ def info_url_handler(environ):
 
 def not_found_handler(environ):
     return b'404', 404, {}
+
+
+class App:
+    def __init__(self):
+        self.handlers = {
+            '/': index_url_handler,
+            '/info/': info_url_handler,
+        }
+    def __call__(self, environ, start_response):
+        current_url_handler = self.handlers.get(
+            environ['PATH_INFO'],
+            not_found_handler
+        )
+        response_text, status_code, extra_headers = current_url_handler(environ)
+
+        status_code_message = '{}{}'.format(
+            Status(status_code).code,
+            Status(status_code).name,
+        )
+        headers = {
+            'Content-Type': 'text/html',
+        }
+        headers.update(extra_headers)
+
+        start_response(
+            status_code_message,
+            list(headers.items()),
+        )
+        return [response_text]
+
+application = App()
+
+application.handlers['/test/'] = application.handlers['/info/']
+
+
